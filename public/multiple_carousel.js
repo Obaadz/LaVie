b9bc2232fdda2160b457324544bf613aa9706349
Obada <obada8411@gmail.com>
2022-12-26 16:42:33 +0200

    refactor: Add 'duplicateCarouselItems' function

--- public/multiple_carousel.js
+++ public/multiple_carousel.js
@@ -1,35 +1,20 @@
-if (typeof carousel_items1 !== null) {
-  let carousel_items1 = document.querySelectorAll("#recipeCarousel1 .carousel-item");
-
-  carousel_items1.forEach((el) => {
-    const minPerSlide = 4;
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        // wrap carousel by using first child
-        next = carousel_items1[0];
+function duplicateCarouselItems(carouselId, minPerSlide) {
+  const carouselItems = document.querySelectorAll(`${carouselId} .carousel-item`);
+  if (typeof carouselItems !== "null") {
+    carouselItems.forEach((el) => {
+      let next = el.nextElementSibling;
+      for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+          // wrap carousel by using first child
+          next = carouselItems[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
       }
-      let cloneChild = next.cloneNode(true);
-      el.appendChild(cloneChild.children[0]);
-      next = next.nextElementSibling;
-    }
-  });
+    });
+  }
 }
 
-if (typeof carousel_items2 !== null) {
-  let carousel_items2 = document.querySelectorAll("#recipeCarousel2 .carousel-item");
-
-  carousel_items2.forEach((el) => {
-    const minPerSlide = 4;
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        // wrap carousel by using first child
-        next = carousel_items2[0];
-      }
-      let cloneChild = next.cloneNode(true);
-      el.appendChild(cloneChild.children[0]);
-      next = next.nextElementSibling;
-    }
-  });
-}
+duplicateCarouselItems("#recipeCarousel1", 4);
+duplicateCarouselItems("#recipeCarousel2", 4);
