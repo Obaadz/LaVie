514b69d678eac544241329ca7b85e185e64545e1
Obada <obada8411@gmail.com>
2022-12-30 02:17:35 +0200

    Fix typo

--- components/about_us/index.tsx
+++ components/about_us/index.tsx
@@ -12,11 +12,11 @@ const AboutUs: FC = () => {
         <div className="row gap-4">
           <div className="col col-lg-6">
             <p>
-              Welcome to La Vie, your number one source for planting. We're dedicated to
-              giving you the very best of plants, with a focus on dependability, customer
-              service and uniqueness. Founded in 2020, La Vie has come a long way from its
-              beginnings in a home office our passion for helping people and give them
-              some advices about how to plant and take care of plants. We now serve
+              Welcome to La Vie, your number one source for planting. We{"'"}re dedicated
+              to giving you the very best of plants, with a focus on dependability,
+              customer service and uniqueness. Founded in 2020, La Vie has come a long way
+              from its beginnings in a home office our passion for helping people and give
+              them some advices about how to plant and take care of plants. We now serve
               customers all over Egypt, and are thrilled to be a part of the eco-friendly
               wing
             </p>
