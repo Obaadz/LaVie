33df2803eefda0cdc3fca0dc8480df58c16b51a7
Obada <obada8411@gmail.com>
2023-01-21 15:35:30 +0200

    refactor: Add typescript to 'Schemas' file

--- server/models/Schemas.ts
+++ server/models/Schemas.ts
@@ -1,13 +1,16 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+import { User } from "../../types/users/User";
 
-const usersSchema = new Schema({
+export interface IUser extends Document, User {}
+
+const usersSchema: Schema<IUser> = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
 });
 
-const Users = mongoose.models.users || mongoose.model("users", usersSchema);
+const Users = mongoose.models.users || mongoose.model<IUser>("users", usersSchema);
 
 const mySchemas = {
   Users,
